d31785795da7a25b72dac968a11f99f913fe95a7
Jodan <jacobusgrobler01@gmail.com>
2024-02-14 11:16:55 +0200

    Odin Rock Paper Scissor Final with bugs, I'm Tired refer to README

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # odin-rock-paper-scissor
 Rock Paper Scissor Game
+
+//What are we doing 
+//Creating a game of rock paper scissor 
+//who is playing
+//person against computer
+
+//How do we make the human play.
+//As per instruction he has to input via the console
+//we give him three choices 
+//he knows them already
+//where will we put its choices
+//he will manually type it in as a parameter as per instructions
+
+
+//how will we let the computer play
+  //How do we give it 3 options
+
+  //The computer must make a random selection between three options
+  //link the options to a number and let it randomly pick a number
+  //where will we put the computer's output
+//place the computer selection in a variable
+
+//how will we make the human and the computer play together
+//we will have to compare their output 
+//how will we compare there output
+//we will have to compare the values of the variables that we created with the original functions 
+//they each have three options how will we cater for a win a loss and a draw
+//how many variable results do we have 
+//rock vs scissor = rock
+//rock vs paper = paper
+//scissor vs paper = scissor
+//samesies = draw
+//lets use the human input as the first operand and work around that
+
+//how do we create a counter for the game to get an ultimate winner
+//we will have to put the count somewhere.
+//then we will have to accumulate the counter
+//we can only accumulate the counter with comparing the if operation results and attaching an action to that result
+
--- script.js
+++ script.js
@@ -1,93 +1,71 @@
-const select0 = "rock";
-const select1 = "paper";
-const select2 = "scissor";
+function playGame() {
 
-let humanScore = 0;
-let computerScore = 0;
+    const getPlayerChoice = function () {
+        const str = prompt();
+        return str.toLowerCase();
+      };
+      let playerSelection = getPlayerChoice();
 
-let totalHuman = [];
-localStorage.setItem("totalHuman", JSON.stringify(totalHuman));
-let totalComputer = [];
-localStorage.setItem("totalComputer", JSON.stringify(totalComputer));
+      const getComputerChoice = function () {
+        const select0 = "rock";
+        const select1 = "paper";
+        const select2 = "scissor";
+      
+        const selector = Math.trunc(Math.random() * 3);
+      
+        if (selector === 1) {
+          return select0;
+        } else if (selector === 2) {
+          return select1;
+        } else return select2;
+      };
+      let computerSelection = getComputerChoice();
 
 
+      let humanScore = 0;
+      let computerScore = 0;
+      console.log(humanScore, computerScore)
+      
+      function playRound(player, computer) {
 
-const getPlayerChoice = function () {
- 
-    const str = prompt();
-    return str.toLowerCase();
     
-};
-const playerSelection = getPlayerChoice();
-console.log(playerSelection);
+        console.log(`Human is ${player},Computer is ${computer}`)
 
-
-const getComputerChoice = function () {
-
-    const selector = Math.trunc(Math.random() * 3) + 1;
-
-  if (selector === 1) {
-    return select0;
-  } else if (selector === 2) {
-    return select1;
-  } else return select2;
-};
-
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-
-
-function playRound (playerChoice, computerChoice) {
-    
-    if (playerChoice === computerChoice) {
-        return( `You Made Drawzies`);
-    } else if (playerChoice === 'rock' && computerChoice === 'scissor') {
-        return (`Human Wins! Computer Sucks`), humanScore++;   
-    } else if (playerChoice === 'scissor' && computerChoice === 'paper') {
-        return(`Human Wins! Computer Sucks`), humanScore++;
-    } else if (playerChoice === 'paper' && computerChoice === 'rock') {
-        return(`Human Wins! Computer Sucks`), humanScore++;
-    } else if (playerChoice === 'rock' && computerChoice === 'paper') {
-        return(`Computer Wins! Human Sucks`), computerScore++;
-    } else if (playerChoice === 'paper' && computerChoice === 'scissor') {
-        return(`Computer Wins! Human Sucks`), computerScore++;
-    } else if (playerChoice === 'scissor' && computerChoice === 'rock') {
-        return( `Computer Wins! Human Sucks`), computerScore++;
-    }   
-    
-}
-
-
-console.log(playRound(playerSelection, computerSelection));
-
-const pointHuman = humanScore; 
-const pointComputer = computerScore;  
-
-const theCount = function(human, computer) {
-   
-    if(human === 1) {
-        totalHuman.push(1)
-     }if (computer === 1) {
-         totalComputer.push(1);
-     } 
-
-     console.log(totalHuman);
-     console.log(totalComputer);
-    
-    function theLoop () {
-        while (human !== 5) {
-            getPlayerChoice()
-        }
-     
+     if (player === computer) {
+      return "Drawzies"
+     }else if (
+                (player === "rock" && computer === "scissors") ||
+                (player === "scissors" && computer === "paper") ||
+                (player === "paper" && computer === "rock")
+              ) 
+              {
+                humanScore = humanScore +1;
+      
+     }else if (
+                (player === "scissor" && computer === "rock") ||
+                (player === "paper" && computer === "scissor") ||
+                (player === "rock" && computer === "paper")
+              )          
+              {
+                computerScore = computerScore +1;}
+       
+     console.log(humanScore, computerScore)
     }
+
+    
+    
+    for (let i = 0; i <= 5; i++) {
+       console.log(playRound(getPlayerChoice(), getComputerChoice()))
+       //console.log(`Human is ${playerSelection},Computer is ${computerSelection}`)
+    } 
+    
+    if (humanScore > computerScore) {
+        console.log(`${humanScore} | ${computerScore} Suck it computer you lucked out` )
+    } if (humanScore < computerScore) {
+      console.log(`${computerScore} | ${humanScore} Suck it human you lucked out` )
+    } else {console.log(`Drawzies`)}
+    
+   
 }
-    
-theCount(pointHuman, pointComputer)
 
-
-
-    
-    
-
-
-
+  playGame()
